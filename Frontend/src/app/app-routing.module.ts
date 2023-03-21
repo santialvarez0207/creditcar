5d13b4769764341f17bfe5caa1d68b51eb6417d6
TheChec <chechoosorio@hotmail.com>
2023-03-21 11:08:10 -0500

    Home-Seeker

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { SeekerComponent } from './seeker/seeker.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path:'login',component: LoginComponent}];
+  {path:'login',component: LoginComponent},
+  {path: 'seeker', component: SeekerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { SeekerComponent } from './seeker/seeker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    SeekerComponent
   ],
   imports: [
     BrowserModule,
