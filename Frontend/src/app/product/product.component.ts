9c47b92d7a39a0711334d35af8b5a089ff34943b
TheChec <chechoosorio@hotmail.com>
2023-03-31 02:11:33 -0500

    Product's ready

--- Frontend/src/app/product/product.component.ts
+++ Frontend/src/app/product/product.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  contador:number = 1;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  Hidden(){
+    if(this.contador==0){
+        document.querySelector("#MyModal")?.classList.add("visto"),
+        this.contador=1}
+
+    else{
+        document.querySelector("#MyModal")?.classList.remove("visto"),
+        this.contador=0
+    }
   }
 
 }
