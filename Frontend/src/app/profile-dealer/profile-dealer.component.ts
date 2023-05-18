c4f82909bd09e0dbb3aa3266a3322b93ca25b358
brandonmartinez2002 <brandonacevedo2002@gmail.com>
2023-05-17 22:34:36 -0500

    avance

--- Frontend/src/app/profile-dealer/profile-dealer.component.ts
+++ Frontend/src/app/profile-dealer/profile-dealer.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-dealer.component.css']
 })
 export class ProfileDealerComponent implements OnInit {
-
+visibleVar:number=1;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+hide(visible:number){
+this.visibleVar=visible;
+let reason=document.getElementsByClassName("Per");
+for(let index=0;index<reason.length;index++){
+  reason[index].classList.remove('visto');
+}
+reason[visible-1].classList.add('visto');
+}
 }
